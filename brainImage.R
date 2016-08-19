8bc69d0c552bd5b4f209157cd5f117434c467f9b
Quinn Wong <qwong1995@gmail.com>
2016-08-19 13:32:47 -0700

    Add files via upload
    Updated script for brain imaging.

---
+++ brainImage.R
@@ -0,0 +1,35 @@
+setwd("C:/Users/Quinn Wong/Documents/SalkProjects")
+library(ggplot2)
+mydata <- read.table("Genes.txt")
+genes <- unique(as.character(mydata$V1))
+######
+slice = 6
+composite <- SpatialEnrichment(slice = 6, genes,reps = 20)
+PlotBrain(composite@composite, Breaks = 9)
+### Find the genes in that region
+#regiongenes <- GetGenes(genes,tissueExp1,region.name="VZ")
+boot <- Boot(tissueExp1 = composite@tissueExp1, tissueExp2 = composite@tissueExp2, random.matrix = composite@random.matrix)
+write.table(x = boot,file = "boot.txt",quote=FALSE)
+
+# Read a file and store into a variable
+bootdata <- read.table("GPe_PaV.boot.tsv", header=TRUE)
+change <- bootdata$change
+
+# Change parameter calculations
+change_sd <- sd(change)*sqrt((length(change)-1)/(length(change)))
+change_mean <- mean(change)
+
+# Create a plot labelling all the tissues compared
+bootdata$geneName <- row.names(bootdata)
+p <- ggplot(bootdata, aes(reorder(geneName, change), change, fill=diff))+
+  geom_bar(stat = "identity")
+p + labs(x = "Tissue", y = "Change", title = "GPe_PaV")
+
+
+## Create a density plot to pinpoint the tissues of interest
+# Red for high difference in change, green for low
+plot <- ggplot(bootdata, aes(change))+
+  geom_density(fill = "grey")+
+  geom_vline(size=2,xintercept = bootdata[bootdata$diff == "high","change"],color = "red")+
+  geom_vline(size=2,xintercept = bootdata[bootdata$diff == "low","change"],color = "green")
+plot + labs(title = "GPe_PaV")
